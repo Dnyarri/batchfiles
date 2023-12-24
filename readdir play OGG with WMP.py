8fad705b5a241644781b2d44a716ce316d38752a
Ilyich the Toad <154424648+Dnyarri@users.noreply.github.com>
2023-12-24 20:26:20 +0300

    initial commit

---
+++ readdir play OGG with WMP.py
@@ -0,0 +1,13 @@
+from tkinter import filedialog
+import glob
+import subprocess
+
+# Open source dir
+sourcedir = filedialog.askdirectory(title='Open DIR to process')
+if (sourcedir == ''):
+    quit()
+
+# Process file list
+for filename in glob.glob(sourcedir + "/**/*.ogg", recursive=True):   # select all OGG files in all subfolders
+    subprocess.run(f'"C:/Program Files/Windows Media Player/wmplayer.exe" "{filename}"', shell=True) # output in quotes for paths with spaces
+    # C:\Program Files\Windows Media Player\wmplayer.exe with changed slashes
---
+++ readdir.py
@@ -0,0 +1,12 @@
+from tkinter import filedialog
+import glob
+import subprocess
+
+# Open source dir
+sourcedir = filedialog.askdirectory(title='Open DIR to process')
+if (sourcedir == ''):
+    quit()
+
+# Print file list
+for filename in glob.glob(sourcedir + "/**/*.*", recursive=True):   # select all files in all subfolders
+    print(f'"Идите в" "{filename}"') # output in quotes for paths with spaces
