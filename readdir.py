c3beb3b3fe5f40a3d51a744f52d1a48e9010aec0
Ilya Razmanov <ilyarazmanov@gmail.com>
2024-01-29 18:37:41 +0300

    update

--- readdir.py
+++ readdir.py
@@ -5,7 +5,9 @@ import glob
 sourcedir = filedialog.askdirectory(title='Open DIR to process')
 if (sourcedir == ''):
     quit()
-
+# Print dir
+print('Куда идти?')
+print (sourcedir)
 # Print file list
 for filename in glob.glob(sourcedir + "/**/*.*", recursive=True):   # select all files in all subfolders
     print(f'"Идите в" "{filename}"') # output in quotes for paths with spaces
