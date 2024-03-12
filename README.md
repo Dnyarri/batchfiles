6853e085b65de3590a58901345c4c103443a78de
Ilya Razmanov <ilyarazmanov@gmail.com>
2024-03-12 11:29:39 +0300

    GUI improments

--- README.md
+++ README.md
@@ -1,10 +1,22 @@
-Some batchfiles are equipped with GUI, suitable as templates for editing.
+# Useful batch scripts for files and dirs processing
+
+Some batchfiles are equipped with GUI, some intended to be used as templates for further editing.
 Most useful are:  
 
-- **dir OPTIVORBIS ogg** - recompress all .ogg within dir and subdirs, using OPTIVORBIS; saves up to 10% of .ogg size.  
+- **dir OPTIVORBIS ogg** - recompress all .ogg within dir and subdirs, using [OPTIVORBIS](https://git.codeproxy.net/OptiVorbis/OptiVorbis/releases); saves up to 10% of .ogg size.  
 
-- **dir LibreOffice rtf2docx** - batch convertion of .rtf, .doc,  .odt and .fb2 files to .docx using LibreOffice. Overcomes clumsy LibreOffice idea of exporting all files into one dir.  
+- **dir LibreOffice rtf2docx** - batch conversion of .rtf, .doc,  .odt and .fb2 files to .docx using [LibreOffice](https://www.libreoffice.org/). Overcomes clumsy LibreOffice idea of exporting all files into one dir.  
 
-- **dir AdvZIP docx** - recompressing .docx after *dir LibreOffice rtf2docx* gives up to 10% space saving.  
+- **dir AdvZIP docx** - recompressing .docx after *dir LibreOffice rtf2docx* using [AdvZIP](https://github.com/amadvance/advancecomp) gives up to 10% space saving.  
+
+- **filenames AdvPNG png** - recompressing .png using [AdvPNG](https://github.com/amadvance/advancecomp) gives up to 5% space saving.  
+
+> [!NOTE]
+> Windows users may rename forementioned files from .py to .pyw to avoid starting console.
 
 - **dir RENAME unflibusta** - batch renaming of files according to patterns, like replacing underscores with spaces, removing digits, and so on. Edit rules pattern to your need.
+
+> [!NOTE]
+> *dir RENAME unflibusta.py* was initially created to batch fix silly filename convention for files downloaded from [Flibusta](https://flibusta.is/) and is supposed to be edited at will as you encounter any other silly file naming scheme.
+
+[Dnyarri website](https://dnyarri.github.io)
--- filenames AdvPNG png.py
+++ filenames AdvPNG png.py
@@ -1,19 +1,92 @@
-# Feeds selected PNG files to advpng.exe for recompression and reducing file size.
-# advpng.exe is available from https://github.com/amadvance/advancecomp
-#
-#   WARNING:
-#   Source files are replaced! No backup, no renaming!
+#!/usr/bin/env python
 
+'''
+Feeds selected PNG files to advpng.exe for recompression and reducing file size.
+advpng.exe is available from https://github.com/amadvance/advancecomp
+
+WARNING:
+Source files are replaced! No backup, no renaming!
+
+'''
+
+__author__ = "Ilya Razmanov"
+__copyright__ = "(c) 2024 Ilya Razmanov"
+__credits__ = "Ilya Razmanov"
+__license__ = "unlicense"
+__version__ = "2024.02.27"
+__maintainer__ = "Ilya Razmanov"
+__email__ = "ilyarazmanov@gmail.com"
+__status__ = "Production"
+
+import os
+
+import subprocess
+
+from tkinter import Tk
+from tkinter import Label
 from tkinter import filedialog
-from subprocess import run
+
+# --------------------------------------------------------------
+# Creating dialog
+
+sortir = Tk()
+sortir.title('Recompressing .PNG...')
+sortir.geometry('+100+100')
+zanyato = Label(sortir, text = 'Starting...', font=("arial", 12), padx=16, pady=10, justify='center')
+zanyato.pack()
+sortir.withdraw()
+
+# Main dialog created and hidden
+# --------------------------------------------------------------
 
 # Open source file list
 sourcefilelist = filedialog.askopenfilenames(title='Select PNG files to recompress', filetypes=[('PNG files', '*.png')]) # filtering for PNG
 if (sourcefilelist == ''):
     quit()
 
+
+# --------------------------------------------------------------
+# Updating dialog
+
+sortir.deiconify()
+zanyato.config(text = 'Allons-y!')
+sortir.update()
+sortir.update_idletasks()
+
+# Dialog shown and updated
+# --------------------------------------------------------------
+
+# --------------------------------------------------------------
+# Creating subprocess startupinfo for hidden console.
+#
+
+startupinfo = None
+if os.name == 'nt':
+    startupinfo = subprocess.STARTUPINFO()
+    startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+
+#
+# now .popen or .run may be used as below:
+# subprocess.run(f'program.exe -switches "{filename}"', startupinfo=startupinfo)
+# --------------------------------------------------------------
+
+
 # Process file list string by string in cycle
 for filename in sourcefilelist:
-    run(f'advpng.exe --recompress --shrink-insane --iter 50 "{filename}"')
+
+    zanyato.config(text = 'Processing ' + filename + '...')      # Updating label, showing processed file name
+    sortir.update()
+    sortir.update_idletasks()
+
+    subprocess.run(f'advpng.exe --recompress --shrink-insane --iter 50 --quiet "{filename}"', startupinfo=startupinfo)
     # output in quotes for paths with spaces
     # --shrink-insane --iter 50 means Zopfli compression with 50 iterations
+
+# --------------------------------------------------------------
+# Destroying dialog
+
+sortir.destroy()
+sortir.mainloop()
+
+# Dialog destroyed and closed
+# --------------------------------------------------------------
