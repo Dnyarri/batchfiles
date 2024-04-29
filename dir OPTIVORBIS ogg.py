610d4eee7458119d33a7e1c0b48febde1dc4de0d
Ilya Razmanov <ilyarazmanov@gmail.com>
2024-04-29 07:44:16 +0300

    replaced os and glob with pathlib

--- dir OPTIVORBIS ogg.py
+++ dir OPTIVORBIS ogg.py
@@ -17,26 +17,20 @@ __author__ = "Ilya Razmanov"
 __copyright__ = "(c) 2024 Ilya Razmanov"
 __credits__ = "Ilya Razmanov"
 __license__ = "unlicense"
-__version__ = "2024.04.27"
+__version__ = "2024.04.29"
 __maintainer__ = "Ilya Razmanov"
 __email__ = "ilyarazmanov@gmail.com"
 __status__ = "Production"
 
 from tkinter import Tk, filedialog, Button, Label, X, BOTH, TOP, BOTTOM
-
 from tkinter.ttk import Progressbar
-
 from tkinter.scrolledtext import ScrolledText
 
-import os
-
-from glob import glob
+from pathlib import Path
 
 import subprocess
 
-# --------------------------------------------------------------
 # Creating dialog
-
 sortir = Tk()
 sortir.title('Recompressing .OGG...')
 sortir.geometry('+100+100')
@@ -62,61 +56,49 @@ butt.pack(fill=X, side=BOTTOM, expand=True)
 
 pogovorit.insert('1.0', 'Allons-y!\n')
 
-sortir.withdraw()
-
-# Main dialog created and hidden
-# --------------------------------------------------------------
+sortir.withdraw()   # Main dialog created and hidden
 
 # Open source dir
 sourcedir = filedialog.askdirectory(title='Open DIR to compress OGG files')
 if (sourcedir == ''):
     quit()
 
-# --------------------------------------------------------------
-# Updating dialog
+path=Path(sourcedir)
 
+# Updating dialog
 sortir.deiconify()
 zanyato.config(text='Allons-y!')
 pogovorit.focus()
 sortir.update()
 sortir.update_idletasks()
 
-# Dialog shown and updated
-# --------------------------------------------------------------
-
-# --------------------------------------------------------------
-# Creating subprocess startupinfo for hidden console.
-#
-
-startupinfo = None
-if os.name == 'nt':
-    startupinfo = subprocess.STARTUPINFO()
-    startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
-
-#
-# now .popen or .run may be used as below:
-# subprocess.run(f'program.exe -switches "{filename}"', startupinfo=startupinfo)
-# --------------------------------------------------------------
+startupinfo = subprocess.STARTUPINFO()
+startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
 
 # Process file list
-for filename in glob(sourcedir + "/**/*.ogg", recursive=True):   # select all OGG files in all subfolders
+for filename in path.rglob('*.ogg', case_sensitive=False):  # cycle through OGG files in subfolders
 
-    zanyato.config(text=f'Processing {filename}...')      # Updating label, showing processed file name
+    zanyato.config(text=f'Processing {filename}...')    # Updating UI, showing processed file name
     progressbar.start(50)
     pogovorit.insert('end -1 chars', f' Starting {filename}...  ')
     pogovorit.see('end')
     sortir.update()
     sortir.update_idletasks()
 
-    os.rename(f"{filename}", "D:/hujwam.ogg")
+    currentfile = Path(filename)        # file to be processed
+    tempfile = Path('D:/hujwam.ogg')    # temp file 'D:/hujwam.ogg'
+    currentfile.replace(tempfile)       # move file to temp
+
     # Note: output in quotes below for paths with spaces
     subprocess.run(f'optivorbis.exe -q D:/hujwam.ogg "{filename}"', startupinfo=startupinfo)
-    os.remove("D:/hujwam.ogg")
-        
+    # optivorbis.exe writes result from temp back to source location
+
     progressbar.start(50)
     pogovorit.insert('end -1 chars', ' Done\n')
     sortir.update()
     sortir.update_idletasks()
+
+tempfile.unlink(missing_ok=True)        # removing temp file
 
 progressbar.stop()
 butt.config(state='normal')
